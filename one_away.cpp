bcd6da1ccb3c18ead3c005d35ec94413a0b1746f
shivam-bharwdaj <sb6377@nyu.edu>
2019-11-23 13:50:37 -0800

    one_away finished

--- one_away.cpp
+++ one_away.cpp
@@ -7,41 +7,65 @@
 #include "string"
 
 using namespace std;
+
 bool one_add(string s1, string s2);
+bool one_remove(string s1, string s2);
 bool one_replace(string s1, string s2);
 
 
-bool one_away(string s1, string s2){
-    int x = s1.length()-s2.length();
-    if(abs(x<=1)){
-        if(x==0)
-            return one_replace(s1, s2);
-        else
-            return one_add(s1, s2);
+bool one_away(string s1, string s2) {
+    int x = s1.length() - s2.length();
+    if (x == 0) {
+        return one_replace(s1, s2);
+    } else if (x == 1) {
+        return one_remove(s1, s2);
+    } else if (x == -1) {
+        return one_add(s1, s2);
+    } else {
+        return false;
     }
-    return false;
+}
+
+bool one_add(string s1, string s2) {
+    bool flag = false;
+    for (int i = 0, j =0; i < s1.length(); ++i, ++j) {
+        if ((flag == false) && (s1[i] != s2[j])) {
+            flag = true; ++i;
+        } else {
+            if ((flag == true) && (s1[i] != s2[i])) return false;
+        }
+    }
+    return true;
 
 }
 
-bool one_add(string s1, string s2){
+bool one_remove(string s1, string s2) {
+    bool flag = false;
+    for (int i = 0, j =0; i < s2.length(); ++i, ++j) {
+        if ((flag == false) && (s1[i] != s2[j])) {
+            flag = true; ++i;
+        } else {
+            if ((flag == true) && (s1[i] != s2[i])) return false;
+        }
+    }
+    return true;
 
-    return false;
 }
 
-bool one_replace(string s1, string s2){
+bool one_replace(string s1, string s2) {
     bool flag = false;
     for (int i = 0; i < s1.length(); ++i) {
-        if((flag == false)&&(s1[i]!=s2[i])){
+        if ((flag == false) && (s1[i] != s2[i])) {
             flag = true;
-        }else{
-            if((flag == true)&&(s1[i]!=s2[i])) return false;
+        } else {
+            if ((flag == true) && (s1[i] != s2[i])) return false;
         }
     }
     return true;
 }
 
-int main(){
-    string s1 = "aaa";
-    string s2 = "aba";
-    cout<<one_away(s1,s2);
+int main() {
+    string s1 = "23";
+    string s2 = "2";
+    cout << one_away(s1, s2);
 }
