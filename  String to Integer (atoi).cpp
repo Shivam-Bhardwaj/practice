6bb8576ba1b95f36745087e8d5eb6c65a49b3a41
shivam-bhardwaj <sb6377@nyu.edu>
2019-12-26 19:13:31 -0800

    atoi test cases added

---  String to Integer (atoi).cpp
+++  String to Integer (atoi).cpp
@@ -4,32 +4,22 @@
 
 using namespace std;
 
-bool isPalindrome(string s) {
-    int j = s.size() - 1;
-    int i = 0;
-    while (i < j) {
-        if (!isalnum(s[i])) {
-            i++;
-            continue;
-        }
-        if (!isalnum(s[j])) {
-            j--;
-            continue;
-        }
-        if (tolower(s[i]) != tolower(s[j])) return false;
-        i++;
-        j--;
-    }
-    return true;
+int myAtoi(string str) {
+
 }
+
 
 int main() {
 //    cout << "here";
 
 
-    assert(isPalindrome("A man, a plan, a canal: Panama") == true);
-    assert(isPalindrome("race a car") == false);
-    assert(isPalindrome("0P") == false);
+    assert(myAtoi("42") == 42);
+    assert(myAtoi("   -42") == -42);
+    assert(myAtoi("4193 with words") == 4193);
+    assert(myAtoi("words and 987") == 0);
+    assert(myAtoi("-91283472332") == -2147483648);
+
+
     cout << "success";
     cin.get();
     cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
