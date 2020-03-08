b9c4f0b046e27833add03187aa2871b778aa43e0
shivam-bhardwaj <shivam.bhardwaj@automataresearch.com>
2020-03-08 03:30:10 -0700

    atoi path modified

--- classes_exp.cpp
+++ classes_exp.cpp
@@ -1,14 +1,9 @@
 #include <stdio.h>
-void slogan();
+
 int main()
 {
     int c = 5;
-    c = slogan();
     printf("\n%d", c);
     return 0;
 }
 
-void slogan( )
-{
-    printf("\n Only the best use C!");
-}
