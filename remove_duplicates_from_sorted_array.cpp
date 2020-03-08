f663f9d39f68e51b48275849e131635bf8961c1e
shivam-bhardwaj <shivam.bhardwaj@automataresearch.com>
2020-03-08 03:40:20 -0700

    debug folder deleted

---
+++ remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+#include "vector"
+#include "cassert"
+
+using namespace std;
+
+int removeDuplicates(vector<int> &nums) {
+  int ctr = 0;
+  int n = nums.size();
+  for (int i = 0; i < n ; ++i) {
+    if (nums[ctr] != nums[i + 1]) {
+      nums[ctr] = nums[i];
+      ctr++;
+    }
+  }
+  for (int j = 0; j < ctr; ++j) {
+    printf("%d ", nums[j]);
+  }
+
+  return ctr;
+}
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  vector<int> list = {{0, 1, 2, 2, 4, 4, 5}};
+  printf("\n%d", removeDuplicates(list));
+  return EXIT_SUCCESS;
+}
