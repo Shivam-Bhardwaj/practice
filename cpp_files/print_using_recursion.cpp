bd251ae4e32e7bc9a1adc035e2a4681461f1d896
Shivam Bhardwaj <sb6377@nyu.edu>
2020-06-09 23:42:27 -0700

    print_using_recursion.cpp added

---
+++ cpp_files/print_using_recursion.cpp
@@ -0,0 +1,29 @@
+//
+// Created by shivam on 6/9/2020.
+//
+
+#include "iostream"
+#include <bits/stdc++.h>
+#include "vector"
+
+using namespace std;
+
+void print(int n) {
+//    if(n == 1){
+//        cout << n << " ";
+//    }
+    if (n > 1) {
+        print(n - 1);
+    }
+    cout << n << " ";
+}
+
+int main() {
+    cout << "here\n\n";
+    print(6);
+
+    cout << "here2";
+    cin.get();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return 0;
+}
