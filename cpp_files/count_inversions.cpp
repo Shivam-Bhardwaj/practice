227d0e21b15bbfa84cbb8a7fd3bd1f2c32210b90
Shivam Bhardwaj <sb6377@nyu.edu>
2020-08-27 18:11:08 -0700

    shifting to Linux

---
+++ cpp_files/count_inversions.cpp
@@ -0,0 +1,100 @@
+// C++ program to Count
+// Inversions in an array
+// using Merge Sort
+#include <bits/stdc++.h>
+#include <iterator>
+#include <fstream>
+
+using namespace std;
+
+size_t _mergeSort(int arr[], int temp[], int left, int right);
+size_t merge(int arr[], int temp[], int left, int mid, int right);
+
+/* This function sorts the input array and returns the
+number of inversions in the array */
+size_t mergeSort(int arr[], int array_size)
+{
+  int temp[array_size];
+  return _mergeSort(arr, temp, 0, array_size - 1);
+}
+
+/* An auxiliary recursive function that sorts the input array and
+returns the number of inversions in the array. */
+size_t _mergeSort(int arr[], int temp[], int left, int right)
+{
+  int mid, inv_count = 0;
+  if (right > left) {
+    /* Divide the array into two parts and
+    call _mergeSortAndCountInv()
+    for each of the parts */
+    mid = (right + left) / 2;
+
+    /* Inversion count will be sum of
+    inversions in left-part, right-part
+    and number of inversions in merging */
+    inv_count += _mergeSort(arr, temp, left, mid);
+    inv_count += _mergeSort(arr, temp, mid + 1, right);
+
+    /*Merge the two parts*/
+    inv_count += merge(arr, temp, left, mid + 1, right);
+  }
+  return inv_count;
+}
+
+/* This funt merges two sorted arrays
+and returns inversion count in the arrays.*/
+size_t merge(int arr[], int temp[], int left,
+          int mid, int right)
+{
+  int i, j, k;
+  size_t inv_count = 0;
+
+  i = left; /* i is index for left subarray*/
+  j = mid; /* j is index for right subarray*/
+  k = left; /* k is index for resultant merged subarray*/
+  while ((i <= mid - 1) && (j <= right)) {
+    if (arr[i] <= arr[j]) {
+      temp[k++] = arr[i++];
+    }
+    else {
+      temp[k++] = arr[j++];
+
+      /* this is tricky -- see above
+      explanation/diagram for merge()*/
+      inv_count = inv_count + (mid - i);
+    }
+  }
+
+  /* Copy the remaining elements of left subarray
+(if there are any) to temp*/
+  while (i <= mid - 1)
+    temp[k++] = arr[i++];
+
+  /* Copy the remaining elements of right subarray
+(if there are any) to temp*/
+  while (j <= right)
+    temp[k++] = arr[j++];
+
+  /*Copy back the merged elements to original array*/
+  for (i = left; i <= right; i++)
+    arr[i] = temp[i];
+
+  return inv_count;
+}
+
+// Driver code
+int main()
+{
+  std::ifstream is("file2.txt");
+  std::istream_iterator<int> start(is), end;
+  std::vector<int> array(start, end);
+  int* arr = &array[0];
+//  int arr[] = { 1, 20, 6, 4, 5 };
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  std::size_t ans = mergeSort(arr, array.size());
+  cout << " Number of inversions are " << ans;
+  return 0;
+}
+
+// This is code is contributed by rathbhupendra
--- cpp_files/merge_sort.cpp
+++ cpp_files/merge_sort.cpp
@@ -5,15 +5,21 @@
 #include "iostream"
 #include <bits/stdc++.h>
 #include "vector"
+#include <iostream>
+#include <iterator>
+#include <fstream>
+#include <vector>
+#include <algorithm> // for std::copy
 
 //using vector<int> = std::vector<int>;
 using namespace std;
-void merge(vector<int> &array, int left, int mid, int right) {
+
+long merge(vector<int> &array, int left, int mid, int right) {
   vector<int> temp_l;
   vector<int> temp_r;
   int left_size = mid - left + 1;
   int right_size = right - mid;
-
+  long inv_ = 0;
   for (int i = 0; i < left_size; ++i) {
     temp_l.push_back(array[i + left]);
   }
@@ -29,6 +35,7 @@ void merge(vector<int> &array, int left, int mid, int right) {
       k++;
     } else {
       array[k] = temp_r.at(j);
+      inv_ += mid - i;
       j++;
       k++;
     }
@@ -37,6 +44,7 @@ void merge(vector<int> &array, int left, int mid, int right) {
     array[k] = temp_l.at(i);
     i++;
     k++;
+    inv_++;
   }
 
   while (j < right_size) {
@@ -44,19 +52,22 @@ void merge(vector<int> &array, int left, int mid, int right) {
     j++;
     k++;
   }
+  return inv_;
 }
 
-void sort(vector<int> &array, int left, int right) {
+long sort(vector<int> &array, int left, int right) {
   //base case
+  long inv = 0;
   if (left < right) {
     // recursive call
     int mid = left + (right - left) / 2;
-    sort(array, left, mid);
-    sort(array, mid + 1, right);
+    inv = sort(array, left, mid);
+    inv += sort(array, mid + 1, right);
 
     // merge
-    merge(array, left, mid, right);
+    inv+= merge(array, left, mid, right);
   }
+  return inv;
 }
 
 bool check(vector<int> &a) {
@@ -69,12 +80,17 @@ bool check(vector<int> &a) {
 }
 
 int main() {
-  vector<int> array{3,2,1};
-
-  sort(array, 0, array.size() - 1);
-  for (int i : array) {
-    std::cout << i << " ";
-  }
+//  vector<int> array{6, 5, 4, 3, 2, 1};
+//  vector<int> array{1, 20, 6, 4, 5};
+//  vector<int> array{2, 4, 1, 3, 5};
+  std::ifstream is("file2.txt");
+  std::istream_iterator<int> start(is), end;
+  std::vector<int> array(start, end);
+  long inv = sort(array, 0, array.size() - 1);
+//  for (int i : array) {
+//    std::cout << i << " ";
+//  }
+  std::cout << std::endl << inv;
   if (check(array)) {
     std::cout << "\nsuccess";
   } else {
