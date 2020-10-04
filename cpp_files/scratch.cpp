8de4d98b05225d43a4d84ef2f91eb926504bee3e
shivam-bhardwaj <sb6377@nyu.edu>
2020-10-04 06:45:35 -0400

    scratch.cpp updated

--- cpp_files/scratch.cpp
+++ cpp_files/scratch.cpp
@@ -8,34 +8,106 @@
 #include "cassert"
 #include "algorithm"
 #include "map"
+#include "stack"
 
 using namespace std;
 
-vector<int> searchInSortedMatrix(vector<vector<int>> matrix, int target) {
-  // Write your code here.
-  size_t row{0}, column{matrix.at(0).size() - 1};
-  while (row < matrix.size() && column >= 0) {
-  if (matrix.at(row).at(column) > target){
-    column--;
-  } else if(matrix.at(row).at(column) < target){
-    row++;
-  } else  return vector<int>{static_cast<int>(row), static_cast<int>(column)};
+
+class LinkedList {
+public:
+  int value;
+  LinkedList *next;
+
+  LinkedList(int value);
+
+  void addMany(vector<int> values);
+
+  vector<int> getNodesInArray();
+};
+
+void create_stack(stack<LinkedList *> &list_stack, LinkedList *head) {
+  while (head != NULL) {
+    list_stack.push(head);
+    head = head->next;
   }
-  return vector<int>{-1, -1};
+}
+
+void pop_stack(stack<LinkedList *> &list_stack, int k) {
+  for (int i = 0; i < k -1; ++i) {
+    list_stack.pop();
+  }
 }
 
 
+void removeKthNodeFromEnd(LinkedList *head, int k) {
+  // Write your code here.
+  LinkedList * start= head;
+  LinkedList * end=head;
+  while(end && k--){
+    end = end->next;
+  }
+  while (end)
+  {
+    start = start->next;
+    end = end->next;
+  }
+
+  if(!end){
+    auto next = start->next;
+//    list_stack.pop();
+    start = start->next;
+  }
+
+}
+
+class TestLinkedList : public LinkedList {
+public:
+  TestLinkedList(int value);
+
+  void addMany(vector<int> values);
+
+  vector<int> getNodesInArray();
+};
+
+LinkedList::LinkedList(int value) {
+  this->value = value;
+  this->next = NULL;
+}
+
+TestLinkedList::TestLinkedList(int value) : LinkedList(value) {
+  this->value = value;
+  this->next = NULL;
+}
+
+void TestLinkedList::addMany(vector<int> values) {
+  LinkedList *current = this;
+  while (current->next != NULL) {
+    current = current->next;
+  }
+  for (int value : values) {
+    current->next = new LinkedList(value);
+    current = current->next;
+  }
+}
+
+vector<int> TestLinkedList::getNodesInArray() {
+  vector<int> nodes{};
+  LinkedList *current = this;
+  while (current != NULL) {
+    nodes.push_back(current->value);
+    current = current->next;
+  }
+  return nodes;
+}
+
 int main() {
   {
-    vector<vector<int>> matrix{
-      {1,  4,   7,   12,  15,  1000},
-      {2,  5,   19,  31,  32,  1001},
-      {3,  8,   24,  33,  35,  1002},
-      {40, 41,  42,  44,  45,  1003},
-      {99, 100, 103, 106, 128, 1004},
-    };
-    vector<int> expected{3, 3};
-    assert(searchInSortedMatrix(matrix, 44) == expected);
+    TestLinkedList test(0);
+    test.addMany({1, 2, 3, 4, 5, 6, 7, 8, 9});
+    TestLinkedList expected(1);
+    expected.addMany({2, 3, 4, 5, 6, 7, 8, 9});
+    removeKthNodeFromEnd(&test, 10);
+    assert(test.getNodesInArray() == expected.getNodesInArray());
     cout << "TEST 0 PASS";
   }
 
