6739287eb798efd1eec31ea89a917d1161e3b09f
shivam-bhardwaj <sb6377@nyu.edu>
2020-10-15 07:01:42 -0400

    refactored

---
+++ finished/balanced_brackets.cpp
@@ -0,0 +1,76 @@
+//
+// Created by shivam on 10/15/20.
+//
+
+#include "iostream"
+#include "vector"
+#include "cassert"
+#include "algorithm"
+
+
+using namespace std;
+
+bool balancedBrackets(string str) {
+  // Write your code here.
+  vector<char> stack_;
+  for (int i{0}; i < str.size(); i++) {
+    char c = str.at(i);
+    switch (c) {
+      case '(': {
+        stack_.push_back(c);
+        break;
+      }
+      case '[': {
+        stack_.push_back(c);
+        break;
+      }
+      case '{': {
+        stack_.push_back(c);
+        break;
+      }
+
+      case ')': {
+        if(stack_.empty()) return false;
+        if (stack_.back() != '(') {
+          return false;
+        } else {
+          stack_.pop_back();
+        }
+        break;
+      }
+
+      case ']': {
+        if(stack_.empty()) return false;
+        if (stack_.back() != '[') {
+          return false;
+        } else {
+          stack_.pop_back();
+        }
+        break;
+      }
+
+      case '}': {
+        if(stack_.empty()) return false;
+        if (stack_.back() != '{') {
+          return false;
+        } else {
+          stack_.pop_back();
+        }
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
+
+  return stack_.empty();
+}
+
+
+int main() {
+  {
+    assert(balancedBrackets("([])(){}(())()()") == true);
+    cout << "TEST 0 PASS";
+  }
+}
