100b672688bcbab12cb8879aa94952d6f6cda9d6
shivam-bhardwaj <sb6377@nyu.edu>
2020-10-25 04:40:50 -0700

    test graph update

--- scratch.cpp
+++ scratch.cpp
@@ -6,14 +6,50 @@
 #include "vector"
 #include "cassert"
 #include "algorithm"
-
+#include "unordered_map"
 
 using namespace std;
 
-///sefd
+class Node{
+  Node* next;
+  int element;
+};
+
+class List {
+
+public:
+  explicit List(int start_element){
+    next = nullptr;
+    element = start_element;
+  }
+  void add_element(int element);
+  void delete_element(int element);
+  void show_elements();
+};
+
+void List::add_element(int element) {
+  if (this->next == nullptr){
+
+  }
+}
+
 int main() {
   {
+    List my_list(0);
+    my_list.add_element(1);
+    my_list.add_element(2);
+    my_list.add_element(3);
 
+    my_list.show_elements();
+
+    my_list.delete_element(2);
+    my_list.show_elements();
+    my_list.add_element(4);
+    my_list.add_element(4);
+    my_list.show_elements();
+
+    my_list.delete_element(4);
+    my_list.show_elements();
     cout << "TEST 0 PASS";
   }
 }
